b1091bf7043d8e8fee663a38cdad97e49f494484
Shivam24S <shivamshrivastav8055@gmail.com>
2025-08-12 23:18:35 +0530

    python lab code added

---
+++ DefaultData.py
@@ -0,0 +1,5 @@
+age = input("Enter age: ")
+print(type(age))             # <class 'str'>
+
+age = int(age)               # Converted to integer
+print(type(age))             # <class 'int'>
---
+++ Py_lab_1.2/user_input.py
@@ -0,0 +1,12 @@
+
+
+name = input("enter your name\n")
+
+hobby = input("enter your hobby\n")
+
+age =input("enter your age\n")
+
+
+print(f"your name is {name},your hobby is {hobby}, and your age is{age}")
+
+

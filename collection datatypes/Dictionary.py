701b8378fea2fc2ba5a771321d01e902f9c49cc7
Shivam24S <shivamshrivastav8055@gmail.com>
2025-09-03 04:45:20 +0530

    typecasting example

--- collection datatypes/Dictionary.py
+++ collection datatypes/Dictionary.py
@@ -1,7 +1,82 @@
 
-# Collection of key-value pairs.
+# # Collection of key-value pairs.
 
-student = {"name":"alice","age":20}
+# student = {"name":"alice","age":20}
+
+
+# # access
+
+# print(student["name"])
+
+# print(student.get("age"))
+
+# # add property
+
+# student["grade"]="A"
+
+# print(student)
+
+# # update 
+
+# student["age"] = 25
+
+# print(student)
+
+# student.update({"age":"21"})
+
+# print(student)
+
+
+# # remove
+
+# student.pop("grade")
+
+# print(student)
+
+# del student["age"]
+# print(student)
+
+# # student.clear()
+# print(student)
+
+
+# print(student.keys())
+
+# print(student.values())
+
+# print(student.items())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+student = {"name":"alice","age":"20"}
 
 
 # access
@@ -10,38 +85,31 @@ print(student["name"])
 
 print(student.get("age"))
 
-# add property
+# add
 
-student["grade"]="A"
-
-print(student)
+student["grade"] = "a"
 
 # update 
 
-student["age"] = 25
+student["grade"]="b"
 
-print(student)
-
-student.update({"age":"21"})
-
-print(student)
-
+student.update({"age":22})
 
 # remove
 
 student.pop("grade")
 
-print(student)
 
 del student["age"]
-print(student)
+
 
 # student.clear()
-print(student)
 
-
-print(student.keys())
+print(
+student.keys())
 
 print(student.values())
 
 print(student.items())
+
+print(student)
---
+++ collection datatypes/typeCasting.py
@@ -0,0 +1,39 @@
+
+# string to integer
+
+num1=5
+
+num2="10"
+
+
+result = num1+int(num2)
+
+print(result)
+
+
+# float to integer
+
+pi = 3.14
+
+whole = int(pi)
+
+print(whole)
+
+
+# tuple to list 
+
+tup=(20,40,5,0,10,2,0)
+
+newList = list(tup)
+
+print(newList)
+
+
+# list to set 
+
+numList = [1,2,1,2,3,5,6,23,5]
+
+newSet = set(newList)
+
+print(newSet)
+

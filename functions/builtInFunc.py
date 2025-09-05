a938c7ed952bcde10cbedaa5f5bab1bd3902fc3d
Shivam24S <shivamshrivastav8055@gmail.com>
2025-09-05 11:31:57 +0530

    built in functions

---
+++ functions/builtInFunc.py
@@ -0,0 +1,91 @@
+# built in function
+
+
+# using print we can log the value as output
+print("hello")
+
+
+# using input function we can take input from users
+input("enter something")
+
+
+# type
+# using type operator we can  get the datatype of our variable
+
+num=10
+
+print(type(num))
+
+# typecasting 
+# using typecasting function we can change the datatypes
+
+num="10"
+
+print(type(num))
+
+int(num)
+
+print(type(num))
+
+
+# str 
+
+num=10
+
+print(type(num))
+
+
+print(str(num))
+
+print(type(num))
+
+
+# float 
+
+num=2
+
+
+print(type(num))
+
+print(float(num))
+
+
+print(type(num))
+
+
+
+word = "python"
+
+print(len(word))
+
+
+# sum 
+
+numbers = [4,5,6,1,3,6,5,2,6]
+
+print(sum(numbers))
+
+
+# sort
+
+print(sorted(numbers))
+
+
+# decescinding
+
+print(sorted(numbers,reverse=True))
+
+
+# max
+
+
+scores=[10,50,6,0,40,80,90]
+
+print(max(scores))
+
+print(min(scores))
+
+
+
+for i in range(1,10):
+    print(i)
